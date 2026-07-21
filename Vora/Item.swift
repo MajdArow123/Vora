02b59914fee1b4581b20612eb5cc2b560fa46896
MajdArow123 <majd.arow123@gmail.com>
2026-07-21 14:26:40 +0300

    Initial Commit

---
+++ Vora/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Vora
+//
+//  Created by Majd Arow on 2026-07-21.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
